744caa6c463959ddcfe5407bc21a4fddd3e3f5b4
AngelaRecchia <angela.recchia@outlook.com>
2021-12-23 12:38:25 +0100

    mongoose

--- src/DB/mongoose.js
+++ src/DB/mongoose.js
@@ -1,40 +1,58 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://localhost:27017/task-app-api')
 
 // user model
 
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
-
-// const me = new User({
-//     name: "Erin",
-//     age: 32
-// })
-
-// me.save().then((me) => {console.log(me)})
-//         .catch((error) => {console.log(error)})
-
-// task model
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
     },
-    completed: {
-        type: Boolean
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error ("Age must me a positive number")
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) throw new Error("Invalid email")
+        }
     }
 })
 
-const shower = new Task({
-    description: "Have a shower",
-    completed: false
+const me = new User({
+    name: "Mia    ",
+    email: "fa@laJJJBJa.it"
 })
 
-shower.save().then((task) => {console.log(task)})
-            .catch((error) => {console.log(error);})
+me.save().then((me) => {console.log(me)})
+        .catch((error) => {console.log(error)})
+
+// task model
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String
+//     },
+//     completed: {
+//         type: Boolean
+//     }
+// })
+
+// const shower = new Task({
+//     description: "Have a shower",
+//     completed: false
+// })
+
+// shower.save().then((task) => {console.log(task)})
+//             .catch((error) => {console.log(error);})
